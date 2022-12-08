53616736376c7e85c9e85fd28b02a651da67c01f
tsukumi <tsukumijima@users.noreply.github.com>
2022-12-08 19:31:13 +0000

    Add: テストするサイトを追加

--- MocrenTestSites.py
+++ MocrenTestSites.py
@@ -11,6 +11,30 @@ class TestSite(TypedDict):
 # テスト対象のサイトの設定
 test_sites: list[TestSite] = [
     {
+        'name': 'つくみ島だより',
+        'url': 'https://blog.tsukumijima.net/',
+        'normal_status_code': 200,
+        'normal_response_data': '<title>つくみ島だより | プログラムとかDTVとか諸々</title>',
+    },
+    {
+        'name': 'つくみ島道具箱',
+        'url': 'https://tools.tsukumijima.net/',
+        'normal_status_code': 200,
+        'normal_response_data': '<title>つくみ島道具箱 | いろんなツールを置いてます</title>',
+    },
+    {
+        'name': 'つくみ島道具箱 - アクセスカウンター',
+        'url': 'https://tools.tsukumijima.net/dream/admin.cgi',
+        'normal_status_code': 200,
+        'normal_response_data': '<form action="./admin.cgi" method="post">',
+    },
+    {
+        'name': 'つくみ島道具箱 - Twitter API のアクセストークンを確認するやつ',
+        'url': 'https://tools.tsukumijima.net/twittertoken-viewer/',
+        'normal_status_code': 200,
+        'normal_response_data': '<title>Twitter API のアクセストークンを確認するやつ | つくみ島道具箱</title>',
+    },
+    {
         'name': '天気予報 API (livedoor 互換)',
         'url': 'https://weather.tsukumijima.net/',
         'normal_status_code': 200,
@@ -45,5 +69,29 @@ test_sites: list[TestSite] = [
         'url': 'https://jikkyo.tsukumijima.net/api/kakolog/jk1?starttime=1606431600&endtime=1606432500&format=json',
         'normal_status_code': 200,
         'normal_response_data': '{"chat":{"thread":"1606417201","no":"2750","vpos":"1440040","date":"1606431601","mail":"184","user_id":"mmJyd4lCsV6e3loLXR0QvZnlnFI","premium":"1","anonymity":"1","date_usec":"373180","content":"六甲おろし歌って"}}',
+    },
+    {
+        'name': 'namami',
+        'url': 'https://jikkyo.tsukumijima.net/namami/',
+        'normal_status_code': 200,
+        'normal_response_data': '<a href="/namami/tv">テレビ</a></li><li><a href="/namami/radio">ラジオ</a></li><li><a href="/namami/bs">BS</a>',
+    },
+    {
+        'name': 'namami - getchannels API',
+        'url': 'https://jikkyo.tsukumijima.net/namami/api/v2/getchannels',
+        'normal_status_code': 200,
+        'normal_response_data': '<channels status="ok">',
+    },
+    {
+        'name': 'shamimomo.net',
+        'url': 'https://shamimomo.net/',
+        'normal_status_code': 200,
+        'normal_response_data': '<title>shamimomo.net</title>',
+    },
+    {
+        'name': 'shamimomo.net - アクセスカウンター',
+        'url': 'https://shamimomo.net/dream/admin.cgi',
+        'normal_status_code': 200,
+        'normal_response_data': '<form action="./admin.cgi" method="post">',
     },
 ]
